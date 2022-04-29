355bd359d8ad9aa469cb6963d754d7925694acd1
Martijn-DN <102958186+Martijn-DN@users.noreply.github.com>
2022-04-29 12:21:46 +0200

    Nieuwe component

---
+++ wt_frontend/src/app/boekenlijst/boekenlijst.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boekenlijst',
+  templateUrl: './boekenlijst.component.html',
+  styleUrls: ['./boekenlijst.component.css']
+})
+export class BoekenlijstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
