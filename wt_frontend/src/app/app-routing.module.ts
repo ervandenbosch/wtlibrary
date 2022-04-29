b7d7192f465e55284fa9c33f786afc63eabcb212
Martijn-DN <102958186+Martijn-DN@users.noreply.github.com>
2022-04-29 12:30:56 +0200

    routing added for boekenlijst

--- wt_frontend/src/app/app-routing.module.ts
+++ wt_frontend/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoekenlijstComponent } from './boekenlijst/boekenlijst.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  {
-    path: 'logout',
-    component: LogoutComponent,
-  },
+  { path: 'boekenlijst', component: BoekenlijstComponent},
+  { path: 'logout', component: LogoutComponent},
 ];
 
 @NgModule({
