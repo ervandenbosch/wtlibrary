a5898a9eddd91c159cb366f77279ba164fb0dce2
Martijn-DN <102958186+Martijn-DN@users.noreply.github.com>
2022-05-02 12:42:14 +0200

    Basic boekenpagina aangemaakt

---
+++ wt_frontend/src/app/boekenpagina/boekenpagina.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boekenpagina',
+  templateUrl: './boekenpagina.component.html',
+  styleUrls: ['./boekenpagina.component.css']
+})
+export class BoekenpaginaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
