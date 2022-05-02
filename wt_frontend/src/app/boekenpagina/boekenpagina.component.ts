35e4cb2592b2512969709153574673730d1540aa
Martijn-DN <102958186+Martijn-DN@users.noreply.github.com>
2022-05-02 15:56:15 +0200

    Variabelen dynamisch gemaakt

--- wt_frontend/src/app/boekenpagina/boekenpagina.component.ts
+++ wt_frontend/src/app/boekenpagina/boekenpagina.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-boekenpagina',
   templateUrl: './boekenpagina.component.html',
   styleUrls: ['./boekenpagina.component.css']
 })
+
 export class BoekenpaginaComponent implements OnInit {
 
-  constructor() { }
+  titel = "Harry Potter and the Goblet of Fire"
+  fotoPad = "../../assets/images/harry.jpg"
+  beschrijving = `Harry Potter en de Vuurbeker (oorspronkelijke titel: Harry Potter and the Goblet of Fire) is de vierde
+  Harry-Potterfilm, gebaseerd op het gelijknamige boek van de Britse schrijfster J.K. Rowling. De film
+  ging in première op 18 november 2005.
+  Mike Newell regisseerde de film en Steve Kloves schreef het script. De themamuziek van John Williams
+  werd opnieuw gebruikt en aangevuld met muziek van Patrick Doyle.
+  De film was genomineerd voor een Academy Award, namelijk die voor de beste artdirection, maar won deze
+  uiteindelijk niet.`
+  categorieen = ["Fantasy"," Avontuur"]
+  beschikbaar = "Beschikbaar"
+
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  handleReservering() {
+    this.router.navigate([''])
   }
 
 }
