ebdddab4caedc7bd015e77c94fe3f64da5f0b045
Martijn-DN <102958186+Martijn-DN@users.noreply.github.com>
2022-05-12 16:25:01 +0200

    12mei

--- wt_frontend/src/app/reserveringen/exemplaar.ts
+++ wt_frontend/src/app/reserveringen/exemplaar.ts
@@ -1,7 +1,10 @@
+import { Boek } from "../boekenlijst/boek";
+
 export interface Exemplaar {
     id: number;
     availability: boolean;
     book_code: string;
     copyid: string;
     staat: string;
+    boek: Boek;
 }
