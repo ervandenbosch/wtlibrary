2be069b769f024b62ab6da05194a0d98d8a8fa3a
Takoala <64777325+Takoala@users.noreply.github.com>
2022-05-14 18:28:51 +0200

    Bug fixes, authorizatie werkt nu volledig.

--- wt_frontend/src/app/app-routing.module.ts
+++ wt_frontend/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { BoekenlijstComponent } from './boekenlijst/boekenlijst.component';
 import { BoekenpaginaComponent } from './boekenpagina/boekenpagina.component';
 import { UseraccountsComponent } from './useraccounts/useraccounts.component';
 import { ProfielpaginaComponent } from './profielpagina/profielpagina.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
@@ -12,21 +11,18 @@ import { BoardUserComponent } from './board-user/board-user.component';
 import { RegisterComponent } from './register/register.component';
 import { ReserveringenComponent } from './reserveringen/reserveringen.component';
 import { LogboekComponent } from './logboek/logboek.component';
-import { CanActivate } from '@angular/router';
-import { LoggedIn, isAdmin } from './service/auth.service';
+import { LoggedIn, isAdmin} from './service/auth.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'user', component: BoardUserComponent, canActivate: [LoggedIn]},
   { path: 'boekenlijst', component: BoekenlijstComponent, canActivate: [LoggedIn]},
   { path: 'boek/:title', component: BoekenpaginaComponent, canActivate: [LoggedIn]},
+  { path: 'profielpagina', component: ProfielpaginaComponent, canActivate: [LoggedIn]},
+  { path: 'profielpagina/:id', component: ProfielpaginaComponent, canActivate: [LoggedIn]},
   { path: 'reserveringen', component: ReserveringenComponent , canActivate: [LoggedIn, isAdmin]},
   { path: 'useraccounts', component: UseraccountsComponent, canActivate: [LoggedIn, isAdmin]},
-  { path: 'profielpagina', component: ProfielpaginaComponent, canActivate: [LoggedIn]},
-  { path: 'profielpagina/:id', component: ProfielpaginaComponent, canActivate: [LoggedIn, isAdmin]},
-
   { path: 'register', component: RegisterComponent, canActivate: [LoggedIn, isAdmin]},
   { path: 'mod', component: BoardModeratorComponent, canActivate: [LoggedIn, isAdmin]},
   { path: 'admin', component: BoardAdminComponent, canActivate: [LoggedIn, isAdmin]},
